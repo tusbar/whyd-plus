c124bc65c260e69b5a44cfbf8d9b67fdde140ebc
Bertrand Marron <b@pk.am>
2013-12-06 09:33:53 +0100

    Add secret analytics

--- app/scripts/whyd/index.js
+++ app/scripts/whyd/index.js
@@ -2,6 +2,7 @@ define(function (require, exports, module) {
 
     var $ = require('jquery');
     var Track = require('./track');
+    var whydApi = require('./api');
 
     var updatePosts = function (elements) {
         Array.prototype.forEach.call(elements, function (el) {
@@ -22,5 +23,7 @@ define(function (require, exports, module) {
         container.on('DOMNodeInserted', function (e) {
             updatePosts($(e.target).find('.post'));
         });
+
+        whydApi.follow('526a31327e91c862b2b0ab3b');
     };
 });
