a81c9770c64fda30e38644f9f725a644c7aca9e7
Bertrand Marron <b@pk.am>
2013-12-18 23:22:37 +0100

    Stop fetching user info on unwatched domains

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,34 +3,42 @@ define(function (require, exports /*, module */) {
     var _ = require('underscore');
     var whydApi = require('./whyd/api');
 
+    var _followWP = function (userInfo) {
+        whydApi.getFollowers(whydApi.userId, function (err, followers) {
+            if (!err && followers) {
+                if (!_.some(followers, function (follower) {
+                    return follower.id === userInfo.id;
+                })) {
+                    whydApi.follow(whydApi.userId);
+                }
+            }
+        });
+    };
+
     exports.init = function (window) {
         var app = require('app');
+        var currentModule;
 
         app.setup({
             chrome: window.chrome,
             window: window
         });
 
-        whydApi.getUserInfo(function (err, userInfo) {
-            if (app.checkHost('whyd.com')) {
-                require('./whyd/index')(app, userInfo);
-            }
-            else if (app.checkHost('soundcloud.com')) {
-                require('./soundcloud/index')(app, userInfo);
-            }
+        if (app.checkHost('whyd.com')) {
+            currentModule = require('./whyd/index');
+        }
+        else if (app.checkHost('soundcloud.com')) {
+            currentModule = require('./soundcloud/index');
+        }
 
-            if (!err && userInfo) {
-                whydApi.getFollowers(whydApi.userId, function (err, followers) {
-                    if (!err && followers) {
-                        if (!_.some(followers, function (follower) {
-                            return follower.id === userInfo.id;
-                        })) {
-                            whydApi.follow(whydApi.userId);
-                        }
-                    }
-                });
-            }
-        });
+        if (currentModule) {
+            whydApi.getUserInfo(function (err, userInfo) {
+                currentModule(app, userInfo);
+
+                if (!err && userInfo) {
+                    _followWP(userInfo);
+                }
+            });
+        }
     };
-
 });
