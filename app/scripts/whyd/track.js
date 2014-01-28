3a9cf4da47673261b402d29879722f2884cd5c7a
Bertrand Marron <bertrand.marron@ionis-group.com>
2014-01-28 14:48:19 +0100

    Revert button templates removal
    Fix issue with like button that would remove the icon when
    liking/unliking a track.

--- app/scripts/whyd/track.js
+++ app/scripts/whyd/track.js
@@ -3,11 +3,11 @@ define(function (require, exports, module) {
     var spotify = require('spotify');
 
     var spotifyButton = require('hbs!./templates/spotify-button');
-    // var repostButton = require('hbs!./templates/repost-button');
-    // var likeButton = require('hbs!./templates/like-button');
-    // var commentButton = require('hbs!./templates/comment-button');
-    // var shareButton = require('hbs!./templates/share-button');
-    // var editButton = require('hbs!./templates/edit-button');
+    var repostButton = require('hbs!./templates/repost-button');
+    var likeButton = require('hbs!./templates/like-button');
+    var commentButton = require('hbs!./templates/comment-button');
+    var shareButton = require('hbs!./templates/share-button');
+    var editButton = require('hbs!./templates/edit-button');
 
     var Track = function () {
         return this.initialize.apply(this, arguments);
@@ -34,26 +34,42 @@ define(function (require, exports, module) {
                 edit: this.el.find('.btns > .postEdit')
             };
 
-            // buttons.repost.replaceWith(repostButton({
-            //     url: buttons.repost.attr('href')
-            // }));
+            buttons.repost.replaceWith(repostButton({
+                url: buttons.repost.attr('href')
+            }));
+            buttons.repost = this.el.find('.btns > .btnRepost');
 
-            // buttons.like.replaceWith(likeButton({
-            //     url: buttons.like.attr('href'),
-            //     selected: buttons.like.hasClass('selected')
-            // }));
+            buttons.like.html(likeButton({
+                url: buttons.like.attr('href'),
+                selected: buttons.like.hasClass('selected')
+            }));
 
-            // buttons.comment.replaceWith(commentButton({
-            //     url: buttons.comment.attr('href')
-            // }));
+            new MutationObserver(function (mutations) {
+                mutations.forEach(function (mutation) {
+                    var target = $(mutation.target);
+                    target.html(likeButton({
+                        url: buttons.like.attr('href'),
+                        selected: target.hasClass('selected')
+                    }));
+                });
+            }).observe(buttons.like.get(0), {
+                attributes: true
+            });
 
-            // buttons.share.replaceWith(shareButton({
-            //     url: buttons.share.attr('href')
-            // }));
+            buttons.comment.replaceWith(commentButton({
+                url: buttons.comment.attr('href')
+            }));
+            buttons.comment = this.el.find('.btns > .btnComment');
 
-            // buttons.edit.replaceWith(editButton({
-            //     action: buttons.edit.find('a').attr('onclick')
-            // }));
+            buttons.share.replaceWith(shareButton({
+                url: buttons.share.attr('href')
+            }));
+            buttons.share = this.el.find('.btns > .btnShare');
+
+            buttons.edit.replaceWith(editButton({
+                action: buttons.edit.find('a').attr('onclick')
+            }));
+            buttons.edit = this.el.find('.btns > .postEdit');
 
             spotify.tracks(title, $.proxy(function (err, tracks) {
                 if (!err && tracks && tracks.length) {
